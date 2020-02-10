a7143902a3de9c19629bb6a814bff498c0c6ced7
Kovalenko Ivan <s1ckretbox@gmail.com>
2020-02-10 20:19:24 +0200

    Solve: Add simple solution for practice problem
    Signed-off-by: Kovalenko Ivan <s1ckretbox@gmail.com>

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,22 +1,51 @@
 #include "Solver.h"
 #include <iostream>
 
-Solver::Solver( ) {
-    std::cout << "I have born!\n";
+#define NDEBUG
+
+#ifdef NDEBUG
+    #define WRITE file
+#endif
+
+#ifndef NDEBUG
+    #define WRITE std::cout
+#endif
+
+Solver::Solver( )
+    : nMaxSlices( 0 )
+    , nTypes( 0 )
+    , slices( nullptr )
+    , slicesSum( 0 ) {
+
 }
 
 Solver::~Solver( ) {
-    std::cout << "I am dead now :(\n";
+    delete[] slices;
 }
 
-void Solver::readDataFromFile( std::ifstream& file ) {
-    std::cout << "I am reading!\n";
+void Solver::readDataFromFile( std::ifstream& file ) {    
+    file >> nMaxSlices;
+    file >> nTypes;
+
+    slices = new unsigned[nTypes];
+
+    for ( unsigned i = 0; i < nTypes; ++i ) {
+        file >> slices[i];
+    }
 }
 
 void Solver::writeDataToFile( std::ofstream& file ) {
-    std::cout << "I am writing!\n";
+    WRITE << pizzas.size() << "\n";
+    for ( auto i = pizzas.rbegin(); i != pizzas.rend(); i++ ) {
+        WRITE << *i << " ";
+    }
 }
 
-void Solver::solve( ) {
-    std::cout << "I am solving!\n";
+void Solver::solve( ) { 
+    for ( int i = nTypes - 1; i > -1; --i ) {
+        if ( slicesSum + slices[i] < nMaxSlices + 1 ) {
+            slicesSum += slices[i];
+            pizzas.push_back( i );
+        }
+    }
 }
