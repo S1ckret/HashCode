7a7feeab4b94eddba036f55e6fdefabe914165cf
Kovalenko Ivan <s1ckretbox@gmail.com>
2020-02-20 22:06:20 +0200

    Solution: basic solution
    Signed-off-by: Kovalenko Ivan <s1ckretbox@gmail.com>

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -71,4 +71,49 @@ void Solver::writeDataToFile( std::ofstream& file ) {
 }
 
 void Solver::solve( ) {
+// The more books, better for us.
+    sort( libraries.begin( ), libraries.end( ), 
+    []( const Library& lhs, const Library& rhs ){
+        return lhs.BooksN > rhs.BooksN;
+    } );
+// Counts by days to sign up.
+// Libraries that will be signed up.
+    ui librariesThatFitInDeadlineN = 0;
+    ui dayCounter = 0;
+    for ( const auto& lib : libraries ) {
+        dayCounter += lib.SignUpTime;
+        if ( dayCounter > daysN) break;
+        ++librariesThatFitInDeadlineN;
+    }
+    
+    dayCounter = 0;
+    ui libCouter = 0;
+    for ( const auto& lib : libraries ) {
+        if ( libCouter > librariesThatFitInDeadlineN ) break;
+        
+        dayCounter += lib.SignUpTime;
+
+        // Count how many books we can order
+        ui daysLeftN = daysN - dayCounter;
+        ui booksToOrderN = lib.BooksPerDay * daysLeftN;
+
+        // Out of border check
+        if ( booksToOrderN > lib.BooksN ) {
+            booksToOrderN = lib.BooksN;
+        }
+
+        // Library parameters
+        LibraryToSignUp libToSignUp;
+        libToSignUp.ID = lib.ID;
+        libToSignUp.BooksN = booksToOrderN;
+
+        // Add books.
+        // Strategy: Liniar. 
+        for ( ui i = 0; i < booksToOrderN; ++i ) {
+            libToSignUp.BooksToScan.push_back( lib.booksID[i] );
+        }
+
+        // Add lib
+        librariesToSignUp.push_back( libToSignUp );
+    }
 }
