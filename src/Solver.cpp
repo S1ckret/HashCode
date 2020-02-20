4f1cf370d750f38625791918785e0f3423a4a097
Kovalenko Ivan <s1ckretbox@gmail.com>
2020-02-20 19:24:53 +0200

    Clean up: Remove PracticeProblem solution
    Signed-off-by: Kovalenko Ivan <s1ckretbox@gmail.com>

--- src/Solver.cpp
+++ src/Solver.cpp
@@ -1,8 +1,6 @@
 #include "Solver.h"
 #include <iostream>
 
-#define NDEBUG
-
 #ifdef NDEBUG
     #define WRITE file
 #endif
@@ -11,41 +9,22 @@
     #define WRITE std::cout
 #endif
 
-Solver::Solver( )
-    : nMaxSlices( 0 )
-    , nTypes( 0 )
-    , slices( nullptr )
-    , slicesSum( 0 ) {
+Solver::Solver( ) {
 
 }
 
 Solver::~Solver( ) {
-    delete[] slices;
+
 }
 
 void Solver::readDataFromFile( std::ifstream& file ) {    
-    file >> nMaxSlices;
-    file >> nTypes;
-
-    slices = new unsigned[nTypes];
-
-    for ( unsigned i = 0; i < nTypes; ++i ) {
-        file >> slices[i];
-    }
+   
 }
 
 void Solver::writeDataToFile( std::ofstream& file ) {
-    WRITE << pizzas.size() << "\n";
-    for ( auto i = pizzas.rbegin(); i != pizzas.rend(); i++ ) {
-        WRITE << *i << " ";
-    }
+   
 }
 
 void Solver::solve( ) { 
-    for ( int i = nTypes - 1; i > -1; --i ) {
-        if ( slicesSum + slices[i] < nMaxSlices + 1 ) {
-            slicesSum += slices[i];
-            pizzas.push_back( i );
-        }
-    }
+   
 }
--- src/Solver.h
+++ src/Solver.h
@@ -12,13 +12,5 @@ public:
     void writeDataToFile( std::ofstream& file );
     void solve();    
 private:
-    unsigned nMaxSlices;
-    unsigned nTypes;
-
-    unsigned * slices;
-
-    unsigned slicesSum;
-
-// Pizza to order
-    std::vector<unsigned> pizzas;
+    
 };
